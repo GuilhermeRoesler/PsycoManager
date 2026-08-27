f1916d90724704a6004f3531ecc3cd6e2120e55c
Guilherme Roesler <guiroesler2@gmail.com>
2026-08-27 16:24:53 -0300

    feat: redirect root route to /pacientes

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='pacientes', permanent=False)),
     path('admin/', admin.site.urls),
     path('pacientes/', include('pacientes.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
